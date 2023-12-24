67e87444d03220834ba3a6576dae4ac9cb633f58
renukabwage <renu.bawage96@gmail.com>
2023-12-24 16:04:25 +0530

    my commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,8 @@
 const mongoose=require('mongoose');
 
 // connect to the database
-mongoose.connect('mongodb://localhost/Auth_Dev');
+//mongoose.connect('mongodb://localhost/Auth_Dev');
+mongoose.connect('mongodb+srv://renubawage96:nxhkx5vYZp9YyrEM@authpro.70yaggh.mongodb.net/?retryWrites=true&w=majority');
 
 // acquire the connection to check if it is successfully
 const db=mongoose.connection;
